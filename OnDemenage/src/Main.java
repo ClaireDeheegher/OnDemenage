9ec466bf63e33ad8d6c12e7fdd58514711bdee51
PenguLover <claire.deheegher@le-campus-numerique.fr>
2024-09-19 16:07:49 +0200

    Update Main.java
    Change code to make sure the number of carts is equal to the input in the end

--- OnDemenage/src/Main.java
+++ OnDemenage/src/Main.java
@@ -6,16 +6,20 @@ public class Main {
         int carton = 34;
         System.out.println("On déménage ! On a 34 cartons et le camion peut prendre 9 cartons.");
 
-        for (int i = 1; i <= carton; i++) {
+        for (int i = 0; i <= carton; i++) {
 
             if(carton>0 && carton<=34){
-                System.out.println("Le déménagement est en cours ! On a effectué " +i+ " voyage(s). On a déplacé " +i*9 +" cartons.");
+                System.out.println("Le déménagement est en cours ! On a effectué " +i+ " voyage(s). Il reste " +carton +" cartons.");
             }
             else{
                 System.out.println ("Erreur");
             }
             carton = carton - 9;
-            //System.out.println(carton);
+            if (carton < 0) {
+                System.out.println("Le déménagement est terminé ! On a effectué " +(i+1)+ " voyage(s). Il reste 0 cartons.");
+                break;
+            }
+
         }
     }
 }
